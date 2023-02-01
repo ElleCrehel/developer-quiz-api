2db3666a64c724c64601e798bf590d66948aee5e
ElleCrehel <ellecrehel@hotmail.be>
2023-02-01 17:29:36 +0100

    difficulty

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,34 +1,71 @@
-// CONSTANTS:
-const catLinux = document.querySelector(".Linux");
-const catBASH = document.querySelector(".BASH");
-const catPHP = document.querySelector(".PHP");
-const catDocker = document.querySelector(".Docker");
-const catHTML = document.querySelector(".HTML");
-const catMySQL = document.querySelector(".MySQL");
-const catWordPress = document.querySelector(".WordPress");
-const catLaravel = document.querySelector(".Laravel");
-const catKubernetes = document.querySelector(".Kubernetes");
-const catJavascript = document.querySelector(".Javascript");
-const catDevOps = document.querySelector(".DevOps");
+// 1) CONSTANTS
+const allCategories = document.querySelectorAll(".btnCategories");
+const categoriePage = document.querySelector(".categories");
+const difficultyPage = document.querySelector(".difficulty");
+const buttongoToDifficulty = document.querySelector(".goToDifficulty")
 
-// SELECT DIFFICULTY
+// 2) NAVIAGTE BETWEEN PAGES
+const pages = [categoriePage, difficultyPage];
+
+// function for navigating trough pages 
+const changeScreen = (destinationScreen) => {
+  destinationScreen.classList.remove("hidden");
+
+  pages.map(page => {
+    if (page !== destinationScreen) {
+      page.classList.add("hidden");
+    }
+  })
+}
+
+// 3) SELECT DIFFICULTY
+for (buttons of allCategories) {
+  buttons.addEventListener('click', async function () {
+    // console.log(this.innerHTML)
+
+    /*Put the chosen categorie in localstorage*/
+    localStorage.setItem('Categorie', this.innerHTML);
+
+    /*Put the chosen categorie in a dynamic variable*/
+    let storeCategorie = localStorage.getItem('Categorie')
+    console.log(`Je koos voor de categorie: ${storeCategorie}`)
+
+     await devQuizApi();
+    /* Check if there is a categorie selected */
+    if (storeCategorie === null) {
+      console.log('choose an option first')
+    } else {
+      buttongoToDifficulty.addEventListener('click', function () {
+        changeScreen(difficultyPage)
+      })
+    }
+  });
+}
+
+
+
+
+
+
+
 
 // FETCH API
+
 const devQuizApi = async () => {
   let url = new URL("https://quizapi.io/api/v1/questions");
   let params = {
-      apiKey: "mJ81K6LQBRLnEo1KU5FM8aKi8EmCsPDEUdvySj8o",
-      difficulty: localStorage.getItem('Difficulty') || '',
-      category: localStorage.getItem('Categorie') || '',
-      limit: localStorage.getItem('Limit') || '',
+    apiKey: "mJ81K6LQBRLnEo1KU5FM8aKi8EmCsPDEUdvySj8o",
+    difficulty: localStorage.getItem('Difficulty') || '',
+    category: localStorage.getItem('Categorie') || '',
+    limit: localStorage.getItem('Limit') || '',
   };
   url.search = new URLSearchParams(params).toString();
   try {
-      let res = await fetch(url);
-      let data = await res.json();
-      return data
+    let res = await fetch(url);
+    let data = await res.json();
+    return data
   } catch (err) {
-      console.log('deze combinatie is niet mogelijk sorry')
+    console.log('deze combinatie is niet mogelijk sorry')
   }
 
 }
